2642939a599463bd63b052d47da3b1a9c690dbf6
Mahek Siddiqui <maheksiddiqui16@gmail.com>
2026-04-25 16:34:04 +0530

    helmet issue

--- src/pages/Blogs/Blog-1.jsx
+++ src/pages/Blogs/Blog-1.jsx
@@ -1,10 +1,11 @@
 import BlogLayout from "./BlogLayout";
+import blog1 from "../../assets/blog1.jpg";
 
 const Blog1 = () => {
   return (
     <BlogLayout
-      title="How to Transfer Human Remains in India"
-      image="/images/blog1.jpg"
+      title="How Are Dead Bodies Transported by Plane?"
+      image={blog1}
     >
       <p>This is full content of blog 1...</p>
       <p>Step-by-step guide...</p>
